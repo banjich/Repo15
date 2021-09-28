5b7f48b18bc8e3e684fd6863f70b36e80f19d62b
banjich <banjich@gmail.com>
2021-09-28 04:57:29 +0200

    Added file index.js and index.html

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+
+const milili = [10,25,4];
+
+
+function myArr(mili) {
+    "use strict";
+    
+    mili = []
+    mili.push(4,10,25)
+
+return mili;
+
+}
+ console.log(myArr(milili));
+
+ module.exports = myArr;
